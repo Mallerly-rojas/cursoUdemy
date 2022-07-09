6fcd97c1b6dfbce65c47320581848fd56472403d
unknown <Mallerly Rojas Cardenas>
2022-07-08 23:20:50 -0500

    en esta clase se comprararan 3 o mas numeros para definir el mayor de ellos

---
+++ src/NumeroMayor.java
@@ -0,0 +1,2 @@
+public class NumeroMayor {
+}
--- src/OperadorTernario.java
+++ src/OperadorTernario.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class OperadorTernario {
-}
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrse su calificacion");
+        double U= entrada.nextDouble();
+
+
+        if (U>=6){
+            System.out.println("aprobada");
+        }else{
+            System.out.println("reprobado");
+        }
+
+
+    }
+    }
+
