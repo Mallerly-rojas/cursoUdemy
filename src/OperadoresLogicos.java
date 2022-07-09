e24207ea350f4cabc5c02dbf857c2f8a93cf92a4
unknown <Mallerly Rojas Cardenas>
2022-07-08 22:48:22 -0500

    se crea clase para ejemplos de precedencia de operadores logicos

--- src/OperadoresLogicos.java
+++ src/OperadoresLogicos.java
@@ -1,2 +1,14 @@
 public class OperadoresLogicos {
+    public static void main(String[] args) {
+        int a=7, b=5;
+        boolean mentira = false;
+        boolean r1= a<b|| a==b;
+        System.out.println(r1);
+        boolean r2 = a>b && a==b;
+        System.out.println(r2);
+
+
+
+
+    }
 }
---
+++ src/PrecedenciaEnOperadoresLogicos.java
@@ -0,0 +1,5 @@
+public class PrecedenciaEnOperadoresLogicos {
+    public static void main(String[] args) {
+
+    }
+}
