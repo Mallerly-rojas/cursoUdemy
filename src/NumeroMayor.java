4e62d219c029b63582cfa4ae32708efb3afd61d8
unknown <Mallerly Rojas Cardenas>
2022-07-08 23:29:08 -0500

    en esta clase veremos el operador instansof

--- src/NumeroMayor.java
+++ src/NumeroMayor.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class NumeroMayor {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrse el primer numero");
+        int a= entrada.nextInt();
+        System.out.println("Ingrse el segundo numero");
+        int b= entrada.nextInt();
+        System.out.println("Ingrse el tercer numero");
+        int c= entrada.nextInt();
+
+        int max;
+        max=(a>b)? a:b;
+        max=(max>c)? max:c;
+        System.out.println(" El maximo entre los tres numeros es " + max);
+
+    }
 }
